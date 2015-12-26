1c88eb99c0deee2fd0800fd65fc6c666d48b1b5c
shawzt <shawzt@gmail.com>
2015-12-26 19:16:07 +0800

    translate the disk status item name

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra/reloader' if development?
 
+DISK_STATUS_NAME_MAP = {
+  "Filesystem" => "文件系统",
+  "Size"       => "容量",
+  "Used"       => "消耗",
+  "Avail"      => "剩余",
+  "Use%"       => "使用百分比",
+  "Mounted"    => "挂载点"
+}.freeze
+
 get '/' do
   slim :index
 end
@@ -13,8 +22,8 @@ end
 
 get '/disk' do
   info = `df -h / /media/nas/`.split("\n")
-  @headers = info.shift.split(" ")
-  @headers.pop
+  @headers = []
+  info.shift.split(" ").each { |name| @headers << DISK_STATUS_NAME_MAP[name] }
   @items = info
   slim :disk
 end
